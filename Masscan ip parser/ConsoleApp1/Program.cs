2f0e23a46faab53a72c407821a71a8cc44b1a977
Pinkcigarette1 <106490885+Pinkcigarette1@users.noreply.github.com>
2022-06-03 14:14:14 +0200

    Add files via upload

---
+++ Masscan ip parser/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		private static void Main(string[] args)
+		{
+			if (File.Exists("Parsed-IP.txt"))
+			{
+				File.Delete("Parsed-IP.txt");
+			}
+			StreamWriter streamWriter = new StreamWriter("Parsed-IP.txt");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Coded by pinkcigarette");
+
+            Console.WriteLine("Enter IPs Path");
+			string[] array = File.ReadAllLines(Console.ReadLine());
+			for (int i = 0; i < array.Length; i++)
+			{
+				string[] array2 = array[i].Split(new char[0]);
+				try
+				{
+					streamWriter.WriteLine(array2[3] + ":" + array2[2]);
+				}
+				catch
+				{
+				}
+			}
+		}
+	}
+}
---
+++ Masscan ip parser/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("545d7b4d-2f51-474f-ab09-d8714b0804a4")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
